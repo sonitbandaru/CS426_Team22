0b3ee4361314562ce2610958d3423821a63f575b
Brian Cheung <bricheung@umass.edu>
2025-03-14 21:59:56 -0400

    added a hero image, reformatted the navbar + styles

--- Surplus/src/components/HomePage/Navbar.tsx
+++ Surplus/src/components/HomePage/Navbar.tsx
@@ -2,7 +2,22 @@ import {Link} from "react-router-dom";
 
 const Navbar=() =>{
   return(
-    <nav style={{ width: "100vw", padding: "1rem", backgroundColor: "#87201d", color: "white", display: "flex", gap: "20px" }}>
+    <nav style={{ 
+    width: "100%", 
+    height: "30px", 
+    padding: "1rem", 
+    backgroundColor: "#87201d", 
+    color: "white", 
+    display: "flex", 
+    gap: "20px", 
+    alignContent: "center", 
+    justifyContent: "center",
+    flexWrap: "wrap",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    zIndex: 1000,
+    }}>
       <Link to="/" style={{ color: "white" }}>Home</Link>
       <Link to="/pickup-scheduler" style={{color: "white"}}>Pickup Scheduler</Link>
       <Link to="/donation-form" style={{ color: "white" }}>Donation Form</Link>
--- Surplus/src/pages/HomePage.tsx
+++ Surplus/src/pages/HomePage.tsx
@@ -1,18 +1,13 @@
 import React from "react";
+import "../styles/HomePage/HomePage.css"; 
+import heroImage from "../assets/hero.png"; 
 
-const HomePage=() => {
-  return(
-    <div>
-      <h2>Welcome to Surplus+</h2>
-      <p>This is the homepage.
-      Surplus + combats food waste and food insecurity by connecting surplus food
-    from dining halls and restaurants with local volunteers who deliver it to shelters 
-    and those in need. Surplus+ will essentially serve as a central hub for volunteers,
-     university staff and businesses to coordinate the pick up and drop off of food that 
-     was intended to be wasted. UMass alone wastes 2.4 million lbs of food annually.
-      The affliction of food insecurity in developed nations is solvable, 
-      We think this could be a good step in that direction!
-      </p>
+const HomePage = () => {
+  return (
+    <div className="homepage-container">
+      <section className="hero">
+        <img src={heroImage} alt="Surplus+ Hero" className="hero-image" />
+      </section>
     </div>
   );
 };
