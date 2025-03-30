ccd3da748790d897bc92bef9cc84a6441a5f258f
sonitbandaru <you@example.com>
2025-03-30 14:11:43 -0400

    Add login page and link to navbar

--- Surplus/src/App.tsx
+++ Surplus/src/App.tsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/HomePage/Navbar";
 import HomePage from "./pages/HomePage";
 import PickupScheduler from "./pages/PickupScheduler";
+
+import LoginPage from "./pages/LoginPage";
+
+
 //import DonationForm from "./pages/DonationForm";
 //import MealSearch from "./pages/MealSearch";
 //import AccountHome from "./pages/Account";
@@ -14,6 +18,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/pickup-scheduler" element={<PickupScheduler />} />
+        <Route path="/login" element={<LoginPage />} />
+
         {/*<Route path="/donation-form" element={<DonationForm />} />
         <Route path="/meal-search" element={<MealSearch />} />
         <Route path="/account-home" element={<Account />} />*/}
--- Surplus/src/components/HomePage/Navbar.tsx
+++ Surplus/src/components/HomePage/Navbar.tsx
@@ -4,6 +4,7 @@ const Navbar=() =>{
   return(
     <nav style={{ width: "100vw", padding: "1rem", backgroundColor: "#87201d", color: "white", display: "flex", gap: "20px" }}>
       <Link to="/" style={{ color: "white" }}>Home</Link>
+      <Link to="/login" style={{color: "white"}}>Login</Link>
       <Link to="/pickup-scheduler" style={{color: "white"}}>Pickup Scheduler</Link>
       <Link to="/donation-form" style={{ color: "white" }}>Donation Form</Link>
       <Link to="/meal-search" style={{color: "white" }}>Meal Search</Link>
