58eccb1cdfebd883457783b098368aa6d43d5b44
EmirhanYil <emirhanayildiz@gmail.com>
2025-03-31 15:32:08 -0400

    Navbar css separated out for more in-depth styling (just centered some stuff)

--- Surplus/src/components/HomePage/Navbar.tsx
+++ Surplus/src/components/HomePage/Navbar.tsx
@@ -1,14 +1,13 @@
 import {Link} from "react-router-dom";
-
+import "../../styles/Navbar.css";
 const Navbar=() =>{
   return(
-    <nav style={{ width: "100vw", padding: "1rem", backgroundColor: "#87201d", color: "white", display: "flex", gap: "20px" }}>
-      <Link to="/" style={{ color: "white" }}>Home</Link>
-      <Link to="/pickup-scheduler" style={{color: "white"}}>Pickup Scheduler</Link>
-      <Link to="/donation-form" style={{ color: "white" }}>Donation Form</Link>
-      <Link to="/meal-search" style={{color: "white" }}>Meal Search</Link>
-      <Link to="/shopping-cart" style={{color: "white" }}>Shopping Cart</Link>
-      <Link to="/account-home" style={{color: "white" }}>Account Home</Link>
+    <nav className="navbar">
+      <Link to="/" className="nav-link" style={{ color: "white" }}>Home</Link>
+      <Link to="/pickup-scheduler" className="nav-link">Pickup Scheduler</Link>
+      <Link to="/donation-form" className="nav-link">Donation Form</Link>
+      <Link to="/meal-search" className="nav-link">Meal Search</Link>
+      <Link to="/account-home" className="nav-link">Account Home</Link>
     </nav>
   );
 };
